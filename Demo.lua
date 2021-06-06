4fd69569dc4f22b1d88577d244299465239e2fb0
Devilish Human <65692341+Unsploit@users.noreply.github.com>
2021-06-06 18:54:06 -0400

    Update Demo.lua

--- Demo.lua
+++ Demo.lua
@@ -1,3 +1,6 @@
+getgenv().autoClick = false
+
+
 local Unsploit = loadstring(game:HttpGet("https://raw.githubusercontent.com/Unsploit-Softwares/Roblox-Scripts/master/Unsploit.lua"))()
 local name = "web.roblox.com"
 local version = {major = 0; minor = 1; subminor = 12; build = 27;}
@@ -5,14 +8,30 @@ local version = {major = 0; minor = 1; subminor = 12; build = 27;}
 local Window = Unsploit.new(name, "Dark")
 
 local tabs = {}
-local pages = {}
 local functions = {}
 local connections = {}
 
 tabs.General = Window:AddTab("General")
+tabs.Autofarm = Window:AddTab("Autofarm")
 tabs.Settings = Window:AddTab("Settings")
+
+tabs.Autofarm:AddButton("Swing Katana", function()
+	local A_1 = "swingKatana"
+	local Event = game:GetService("Players").LocalPlayer.ninjaEvent
+	Event:FireServer(A_1)
+
+end)
 
 tabs.Settings:AddLabel("Developers")
 tabs.Settings:AddLabel("UI Library - Unsploit")
 tabs.Settings:AddLabel("Unsploit Hub - IHoldOnI & Unsploit")
 tabs.Settings:AddLabel("Copyright(c) 2021 Unsploit Softwares.")
+
+
+functions.Autofarm._autoClick = function()
+	while getgenv().autoClick do
+		local A_1 = "swingKatana"
+		local Event = game:GetService("Players").LocalPlayer.ninjaEvent
+		Event:FireServer(A_1)
+	end
+end

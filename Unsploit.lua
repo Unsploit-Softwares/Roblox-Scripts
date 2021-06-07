30280adc269d2a6a4e9aac70364cd56b6de36acc
Devilish Human <65692341+Unsploit@users.noreply.github.com>
2021-06-07 19:22:52 -0400

    Update Unsploit.lua

--- Unsploit.lua
+++ Unsploit.lua
@@ -45,6 +45,43 @@ local toHSV = Color3.toHSV
 
 local Library: Unsploit = {
 	Notification = loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterNotifications.lua"))();
+	Draggify = function(Frame)
+		dragToggle = nil
+		dragSpeed = .25 -- You can edit this.
+		dragInput = nil
+		dragStart = nil
+		dragPos = nil
+		
+		function updateInput(input)
+			Delta = input.Position - dragStart
+			Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + Delta.X, startPos.Y.Scale, startPos.Y.Offset + Delta.Y)
+			game:GetService("TweenService"):Create(Frame, TweenInfo.new(.25), {Position = Position}):Play()
+		end
+		
+		Frame.InputBegan:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
+			dragToggle = true
+			dragStart = input.Position
+			startPos = Frame.Position
+			input.Changed:Connect(function()
+					if (input.UserInputState == Enum.UserInputState.End) then
+						dragToggle = false
+					end
+				end)
+			end
+		end)
+		Frame.InputChanged:Connect(function(input)
+			if (input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch) then
+				dragInput = input
+			end
+		end)
+		
+		game:GetService("UserInputService").InputChanged:Connect(function(input)
+			if (input == dragInput and dragToggle) then
+				updateInput(input)
+			end
+		end)
+	end;
 	Version = {
 		maj = 0,
 		min = 1,
@@ -131,7 +168,7 @@ function Library.new(name, theme)
 	Main.Position = UDim2.new(0.5, 0, 0.5, 0)
 	Main.Size = UDim2.new(0, 500, 0, 300)
 
-	Draggify(Library._instance:FindFirstChild("Main"))
+	Library.Draggify(Main)
 
 	TopBar.Name = "TopBar"
 	TopBar.Parent = Main
